19bcf22fcbae5362639b0d6a582eeaceae1c3c4b
AllonKleinLab <calebsw@gmail.com>
2016-12-09 20:54:03 -0500

    added Use_genes

--- preprocessing_python.py
+++ preprocessing_python.py
@@ -143,86 +143,7 @@ def get_PCA(E,numpc):
 
 #========================================================================================#
 def save_spring_dir(E,D,k,gene_list,project_directory, custom_colors={},cell_groupings={}):
-	''' 
-	##############################################
-	Builds a SPRING project directory and transforms data into SPRING-readable formats
-	
-	Input (Required)
-		E                  = (numpy array) matrix of gene expression. Rows correspond to 
-		                     celles and columns correspond to genes. 
-		D                  = (numpy array) distance matrix for construction of knn graph.
-		                     Any distance matrix can be used as long as higher values
-		                     correspond to greater distances. 
-		k                  = Number of edges assigned to each node in knn graph
-		gene_list          = An ordered list of gene names with length length E.shape[1]
-		project_directory  = Path to a directory where SPRING readable files will be 
-							 written. The directory does not have to exist before running 
-							 this function.
-	
-	Input (Optional)
-		cell_groupings     = Dictionary with one key-value pair for each cell grouping. 
-							 The key is the name of the grouping (e.g. "SampleID") and 
-							 the value is a list of labels (e.g. ["sample1","sample2"...])
-							 If there are N cells total (i.e. E.shape[0] == N), then the
-							 list of labels should have N entries. 
-		custom_colors      = Dictionary with one key-value pair for each custom color. 
-							 The key is the name of the color track and the value is a 
-							 list of scalar values (i.e. color intensities). If there are 
-							 N cells total (i.e. E.shape[0] == N), then the list of labels 
-							 should have N entries. 	
-	##############################################
-	'''
-	os.system('mkdir '+project_directory)
-	if not project_directory[-1] == '/': project_directory += '/'
-	# Build graph
-	#print 'Building graph'
-	edges = get_knn_edges(D,k)
-
-	# save genesets
-	#print 'Saving gene sets'
-	custom_colors['Uniform'] = np.zeros(E.shape[0])
-	write_color_tracks(custom_colors, project_directory+'color_data_gene_sets.csv')
-	all = []
-	
-	# save gene colortracks
-	#print 'Savng coloring tracks'
-	os.system('mkdir '+project_directory+'gene_colors')
-	II = len(gene_list) / 50 + 1
-	for j in range(50):	
-		fname = project_directory+'/gene_colors/color_data_all_genes-'+repr(j)+'.csv'
-		all_gene_colors = {g : E[:,i+II*j] for i,g in enumerate(gene_list[II*j:II*(j+1)]) if np.mean(E[:,i+II*j]) > 0.05}
-		write_color_tracks(all_gene_colors, fname)
-		all += all_gene_colors.keys()
-	
-	# Create and save a dictionary of color profiles to be used by the visualizer
-	#print 'Color stats'
-	color_stats = {}
-	for i in range(E.shape[1]):
-		mean = np.mean(E[:,i])
-		std = np.std(E[:,i])
-		max = np.max(E[:,i])
-		centile = np.percentile(E[:,i],99.6)
-		color_stats[gene_list[i]] = (mean,std,0,max,centile)
-	for k,v in custom_colors.items():
-		color_stats[k] = (0,1,np.min(v),np.max(v)+.01,np.percentile(v,99))
-	json.dump(color_stats,open(project_directory+'/color_stats.json','w'),indent=4, sort_keys=True)
-		
-
-	# save cell labels
-	#print 'Saving categorical color data'
-	categorical_coloring_data = {}
-	for k,labels in cell_groupings.items():
-		label_colors = {l:frac_to_hex(float(i)/len(set(labels))) for i,l in enumerate(list(set(labels)))}
-		categorical_coloring_data[k] = {'label_colors':label_colors, 'label_list':labels}
-	json.dump(categorical_coloring_data,open(project_directory+'/categorical_coloring_data.json','w'),indent=4)
-	
-	
-	#print 'Writing graph'
-	nodes = [{'name':i,'number':i} for i in range(E.shape[0])]
-	edges = [{'source':i, 'target':j, 'distance':0} for i,j in edges]
-	out = {'nodes':nodes,'links':edges}
-	open(project_directory+'graph_data.json','w').write(json.dumps(out,indent=4, separators=(',', ': ')))
-
+	pass
 #========================================================================================#
 def row_sum_normalize(A):
 	print A.shape
@@ -250,4 +171,3 @@ def write_color_tracks(ctracks, fname):
 def frac_to_hex(frac):
 	rgb = tuple(np.array(np.array(plt.cm.jet(frac)[:3])*255,dtype=int))
 	return '#%02x%02x%02x' % rgb
-
